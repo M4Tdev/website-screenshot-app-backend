000daa94944b61cdfab9ce08ebf61ece8693545d
M4T <mateo.lesiuk@gmail.com>
2019-07-22 16:45:41 +0200

    Add best launch options for linux

--- server.js
+++ server.js
@@ -22,7 +22,10 @@ const validateUrl = url => {
 };
 
 const getScreenshot = async url => {
-  const browser = await puppeteer.launch({ defaultViewport: { width: 1920, height: 1080 } });
+  const browser = await puppeteer.launch({
+    defaultViewport: { width: 1920, height: 1080 },
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
 
   const page = await browser.newPage();
 
