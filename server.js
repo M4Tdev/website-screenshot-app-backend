9e38f02ac0a01cb09d8d645b4b18b1e2ccf199a4
M4T <mateo.lesiuk@gmail.com>
2019-07-22 10:05:52 +0200

    Moved some code to their own functions, screenshot maker works

--- server.js
+++ server.js
@@ -1,23 +1,50 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
+const cors = require('cors');
+const { URL } = require('url');
 
 const app = express();
+
+app.use(cors());
+
+const stripHTTP = url => {
+  const myUrl = new URL(url);
+  return myUrl.hostname;
+};
+
+const validateUrl = url => {
+  if (url.includes('http://') || url.includes('https://')) {
+    return url;
+  }
+
+  return `http://${url}`;
+};
 
 const getScreenshot = async url => {
   const browser = await puppeteer.launch({ defaultViewport: { width: 1920, height: 1080 } });
 
   const page = await browser.newPage();
-  await page.goto(`http://${url}`);
+
+  const validatedUrl = validateUrl(url);
+
+  const strippedUrl = stripHTTP(validatedUrl);
+
+  await page.goto(validatedUrl);
   await page.waitFor(1000);
-  await page.screenshot({ path: `./screenshots/${url}.png` });
+  await page.screenshot({ path: `./screenshots/${strippedUrl}.png` });
+  await browser.close();
+
+  return strippedUrl;
 };
 
 app.use('/:url', async (req, res) => {
   const { url } = req.params;
 
-  await getScreenshot(url);
+  const decodedUrl = decodeURIComponent(url);
 
-  res.download(`./screenshots/${url}.png`);
+  const downloadName = await getScreenshot(decodedUrl);
+
+  res.send({ status: 'done', downloadUrl: `${req.headers.referer}screenshots/${downloadName}` });
 });
 
 const PORT = process.env.PORT || 5000;
